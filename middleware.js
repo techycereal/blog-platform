7b9883fd3fd222822707b39c635ad4129dc2dda4
techycereal <alexjmilliken@outlook.com>
2024-08-22 15:49:51 -0400

    updated middleware

--- middleware.js
+++ middleware.js
@@ -6,7 +6,8 @@ import { NextResponse } from 'next/server';
 const userCache = new Map();
 
 export async function middleware(request) {
-  const response = authMiddleware(request, {
+  // Apply the authentication middleware
+  const authResponse = authMiddleware(request, {
     loginPath: "/api/login",
     logoutPath: "/api/logout",
     apiKey: clientConfig.apiKey,
@@ -16,53 +17,51 @@ export async function middleware(request) {
     serviceAccount: serverConfig.serviceAccount,
   });
 
+  // Get tokens and handle user data
   const tokens = await getTokens(request.cookies, {
     apiKey: clientConfig.apiKey,
     cookieName: serverConfig.cookieName,
     cookieSignatureKeys: serverConfig.cookieSignatureKeys,
     serviceAccount: serverConfig.serviceAccount,
   });
+
   if (tokens) {
     const { uid } = tokens.decodedToken;
-    console.log(uid)
     if (!userCache.has(uid)) {
       try {
-        const url = `https://${process.env.BASE_URL}/api/user/${uid}`
-        console.log(url)
+        const url = `https://${process.env.BASE_URL}/api/user/${uid}`;
         const apiResponse = await fetch(url);
         if (!apiResponse.ok) {
           throw new Error('Failed to fetch user data');
         }
-        
         const userData = await apiResponse.json();
-        console.log(userData)
         userCache.set(uid, userData.data);
-  
+
         // Create response object and set cookie
         const res = NextResponse.next();
         res.cookies.set('userUid', JSON.stringify(userData.data), {
-          path: '/', // Ensure the cookie is available on all routes
+          path: '/',
           maxAge: 60 * 60 * 24, // 1 day
         });
-  
+
         return res;
       } catch (error) {
         console.error("Error fetching user data from local API:", error);
       }
     } else {
-      const userData = userCache.get(uid)
+      const userData = userCache.get(uid);
       const res = NextResponse.next();
-        res.cookies.set('userUid', JSON.stringify(userData), {
-          path: '/', // Ensure the cookie is available on all routes
-          maxAge: 60 * 60 * 24, // 1 day
-        });
-  
-        return res;
+      res.cookies.set('userUid', JSON.stringify(userData), {
+        path: '/',
+        maxAge: 60 * 60 * 24, // 1 day
+      });
+
+      return res;
     }
   }
-  
 
-  return response;
+  // Return the default response if not authenticated
+  return authResponse;
 }
 
 export const config = {
