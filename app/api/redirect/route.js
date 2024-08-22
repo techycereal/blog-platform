ddc57685fa6116b9d26a176670c8add2d3037c9e
techycereal <alexjmilliken@outlook.com>
2024-08-22 16:19:01 -0400

    test please

--- app/api/redirect/route.js
+++ app/api/redirect/route.js
@@ -1,7 +1,9 @@
 // Example API route: /api/some-route
 import { NextResponse } from 'next/server';
+
 export const dynamic = 'force-dynamic';
 
 export async function GET(request) {
-    return NextResponse.redirect(307, '/')   
+    // Perform a 307 redirect to the root ('/')
+    return NextResponse.redirect(new URL('/', request.url), 307);
 }
