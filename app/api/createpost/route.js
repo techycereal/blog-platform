7ad2e2eeda2de8fad98ff79715aa0c42c776a1fd
techycereal <alexjmilliken@outlook.com>
2024-09-04 11:57:37 -0400

    error fix

--- app/api/createpost/route.js
+++ app/api/createpost/route.js
@@ -82,9 +82,3 @@ export async function POST(request) {
     return NextResponse.json({ error: 'An error occurred while creating the post.' }, { status: 500 });
   }
 }
-
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
